f9f52d04f3b0816e83e9f28d4bbff461237a27f3
vaishalijillewar <vaishalijillewar@gmail.com>
2023-07-07 15:41:28 +0530

    this is my 10th commit

---
+++ set.py
@@ -0,0 +1,6 @@
+data = {10,20,30,20,30,' python'}  #unordered collection.
+lis = ['apple', 'samsung', 'apple']
+data.update(lis) #used to update the set with other data types.
+print(data)
+data.discard('apple') #exact element pass
+print(data)
